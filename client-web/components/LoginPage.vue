19ba84499dfc9ae3db2a24f80ed99b2e9224d79c
Miguel Mendes <miguelangello13@gmail.com>
2022-03-10 17:42:26 +0000

    fix login copy

--- client-web/components/LoginPage.vue
+++ client-web/components/LoginPage.vue
@@ -2,8 +2,10 @@
   <div class="flex flex-col justify-center items-center space-y-10">
     <div class="flex flex-col justify-center items-center space-y-4">
       <Logo class="w-96" />
-      <h3 class="text-xl">A place where you can buy and sell photos.</h3>
-      <p>Encrypted images, which other to see them have to buy it</p>
+      <h3 class="text-xl">
+        A dApp built on the NEAR Protocol and IPFS with Nuxt.js
+      </h3>
+      <p>Where anyone can buy and sell their images (not NFTs).</p>
       <p>Please login and start sell and buy them.</p>
     </div>
     <NearSessionButton />
